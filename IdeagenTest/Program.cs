e64fd22df673fb1b09d180d3bfb9532b0bc3bd95
amnan syahmi <amnansyahmi@gmail.com>
2021-06-24 00:42:48 +0800

    Add project files.

---
+++ IdeagenTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdeagenTest
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(@"Please Enter your equation : ");
+            string equation = Console.ReadLine();
+
+            double result = Convert.ToDouble(new DataTable().Compute(equation, null));
+
+            var stringResult = result.ToString();
+            Console.WriteLine("Result: " + stringResult);
+            Console.ReadLine();
+        }
+    }
+}
